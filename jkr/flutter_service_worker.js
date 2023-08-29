8cd4ff895935dbf972458c51050ed4e6a61cf71c
ChiragShyanis <109713366+ChiragShyanis@users.noreply.github.com>
2023-08-29 12:10:23 +0530

    version 3

--- jkr/flutter_service_worker.js
+++ jkr/flutter_service_worker.js
@@ -104,9 +104,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "d83d9c91a46eccf495e4fb95be7d6af4
 "flutter.js": "6b515e434cea20006b3ef1726d2c8894",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "70f6e52258f829444df91142582dffe9",
-"/": "70f6e52258f829444df91142582dffe9",
-"main.dart.js": "33ceae2a0bd9c6a5815ffb8fb92e4272",
+"index.html": "7f5c686cc508d1ba4be6f062204eacec",
+"/": "7f5c686cc508d1ba4be6f062204eacec",
+"main.dart.js": "e7d0de16ad6124899340a5b191719447",
 "manifest.json": "47d1c9f16f2093b9b8f660922e274443",
 "version.json": "7f358a298adc3d5329a7f650672cb59b"};
 // The application shell files that are downloaded before a service worker can
